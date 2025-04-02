687f7839f436220d93bc8882c3907537d0cd871c
AntonioViscomi <antonio.viscomi@iit.it>
2025-04-02 12:17:38 +0200

    Added plot functions

--- MultiJoint_Setup_Analysis_Script/Encoders/Aksim/AksimEncoder.m
+++ MultiJoint_Setup_Analysis_Script/Encoders/Aksim/AksimEncoder.m
@@ -3,6 +3,11 @@ classdef AksimEncoder < Encoder
     
     properties (Access = public)
         Diagnostic
+        rawDiagnosticData
+        timestamps
+        crc__
+        c2l__
+        invalid_data__
     end %end of properties
 
     methods
@@ -16,7 +21,7 @@ classdef AksimEncoder < Encoder
             % Extracts diagnostic information from the experiment and classifies errors.
 
             % Retrieve diagnostic data
-            [rawDiagnosticData, numberOfSamples] = experiment.GetDiagnosticData();
+            [obj.rawDiagnosticData, numberOfSamples] = experiment.GetDiagnosticData();
 
             % Initialize struct to count errors
             counts = struct( ...
@@ -27,32 +32,46 @@ classdef AksimEncoder < Encoder
             );
 
             % Iterate over diagnostic samples
+            obj.crc__ = zeros(1, numberOfSamples);
+            obj.c2l__ = zeros(1, numberOfSamples);
+            obj.invalid_data__ = zeros(1, numberOfSamples);
+
             for d = 1:numberOfSamples
-                errorCode = bitand(rawDiagnosticData(1, d), double(0xFFFF));
+                errorCode = bitand(obj.rawDiagnosticData(1, d), double(0xFFFF));
 
                 switch errorCode
                     case 0x01 % CRC Error
                         counts.crc = counts.crc + 1;
+                        obj.crc__(d) = 1;
 
                     case 0x02 % C2L Error
                         counts.c2l = counts.c2l + 1;
+                        obj.c2l__(d) = 1;
 
                     case 0x03 % C2L + CRC Error
                         counts.crc = counts.crc + 1;
+                        obj.crc__(d) = 1;
 
                     case 0x04 % Invalid Data Error
                         counts.invalid_data = counts.invalid_data + 1;
+                        obj.invalid_data__(d) = 1;
 
                     case 0x05 % CRC + Invalid Data
                         counts.crc = counts.crc + 1;
+                        obj.crc__(d) = 1;
 
                     case 0x06 % C2L + Invalid Data
                         counts.c2l_invalid_data = counts.c2l_invalid_data + 1;
 
                     case 0x07 % CRC + C2L + Invalid Data
                         counts.crc = counts.crc + 1;
+                        obj.crc__(d) = 1;
                 end
             end
+
+            obj.crc__(obj.crc__ == 0) = nan;
+            obj.c2l__(obj.c2l__ == 0) = nan;
+            obj.invalid_data__(obj.invalid_data__ == 0) = nan;
 
             % Compute percentages
             obj.Diagnostic.counts.crc = counts.crc;
@@ -90,6 +109,27 @@ classdef AksimEncoder < Encoder
             fprintf("| C2L warning            | %.4f%%     |\n", c2l);
             fprintf("| Invalid data error     | %.4f%%     |\n", invalid_data);
             fprintf("| Sum of the errors      | %.4f%%     |\n", total_error);
+
+        end
+
+        function plotDiagnosticOnRawData(obj, experiment)
+            
+            obj.timestamps = experiment.GetTimestamps();
+            rawData = experiment.GetRawData();
+
+            plot(obj.timestamps, rawData(1, :))
+            hold on
+            plot(obj.timestamps, obj.crc__.*rawData(1, :), 'r*')
+            plot(obj.timestamps, obj.c2l__.*rawData(1, :), 'g*')
+            plot(obj.timestamps, obj.invalid_data__.*rawData(1, :), 'b*')
+            hold off
+            title('Encoder raw data (Joint position)');
+            subtitle(['Joint position, (joint speed: ', 'number', ' $[\frac{deg}{sec}$])'], 'Interpreter', 'latex');
+            axis([-inf, inf, -2^15, 2.01^19]);
+            xlabel('Time (seconds)');
+            lgd = legend('Joint position', 'CRC error', 'C2L error', 'Invalid Data error');
+            lgd.FontSize = 5; lgd.Location = 'north'; lgd.Orientation = 'horizontal';
+            set(gca,'color', [0.9 0.9 0.9]);
 
         end
 
