a4f844dc4dd9ee3f12eb534fa43a3945e230293b
AntonioViscomi <antonio.viscomi@iit.it>
2025-04-02 14:21:17 +0200

    Fixed figure generation behavior

--- MultiJoint_Setup_Analysis_Script/UtilityFunctions/add_resources_paths.m
+++
@@ -1,9 +0,0 @@
-%% Add directories to MATLAB path
-addpath('Encoders');
-addpath('Encoders/Aksim');
-addpath('Encoders/Amo');
-addpath('Joint');
-addpath('Motor');
-addpath('Experiment');
-addpath('Experiment/UtilityScripts');
-addpath('Data');
